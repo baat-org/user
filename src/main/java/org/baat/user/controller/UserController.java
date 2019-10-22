c5005b5d526e3990efaf288501ad74fb19b45eb5
Sachin Goyal <sachin@fanduel.com>
2019-10-22 23:21:29 +0100

    Added logging

--- src/main/java/org/baat/user/controller/UserController.java
+++ src/main/java/org/baat/user/controller/UserController.java
@@ -4,12 +4,10 @@ import org.baat.core.transfer.user.SignupRequest;
 import org.baat.core.transfer.user.UserCredentials;
 import org.baat.core.transfer.user.UserInfo;
 import org.baat.user.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Set;
@@ -19,43 +17,45 @@ import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 
 @RestController
 public class UserController {
+    private final static Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 
-	@Autowired
-	UserService userService;
+    @Autowired
+    UserService userService;
 
-	@CrossOrigin
-	@RequestMapping(value = "/authenticate", method = PUT)
-	public String authenticate(@RequestBody final UserCredentials credentials) {
-		return userService.authenticate(credentials);
-	}
+    @CrossOrigin
+    @RequestMapping(value = "/authenticate", method = PUT)
+    public String authenticate(@RequestBody final UserCredentials credentials) {
+        return userService.authenticate(credentials);
+    }
 
-	@CrossOrigin
-	@RequestMapping(value = "/signup", method = PUT)
-	public String signup(@RequestBody final SignupRequest signupRequest) {
-		return userService.signup(signupRequest);
-	}
+    @CrossOrigin
+    @RequestMapping(value = "/signup", method = PUT)
+    public String signup(@RequestBody final SignupRequest signupRequest) {
+        LOGGER.info("Signing up {}", signupRequest.getEmail());
+        return userService.signup(signupRequest);
+    }
 
-	@CrossOrigin
-	@RequestMapping(value = "/validateUserToken/{userToken}", method = GET)
-	public boolean validateUserToken(@PathVariable("userToken") final String userToken) {
-		return userService.validateUserToken(userToken);
-	}
+    @CrossOrigin
+    @RequestMapping(value = "/validateUserToken/{userToken}", method = GET)
+    public boolean validateUserToken(@PathVariable("userToken") final String userToken) {
+        return userService.validateUserToken(userToken);
+    }
 
-	@CrossOrigin
-	@RequestMapping(value = "/userForToken/{userToken}", method = GET)
-	public UserInfo userForToken(@PathVariable("userToken") final String userToken) {
-		return userService.getUserForToken(userToken);
-	}
+    @CrossOrigin
+    @RequestMapping(value = "/userForToken/{userToken}", method = GET)
+    public UserInfo userForToken(@PathVariable("userToken") final String userToken) {
+        return userService.getUserForToken(userToken);
+    }
 
-	@CrossOrigin
-	@RequestMapping(value = "/users", method = GET)
-	public List<UserInfo> users() {
-		return userService.getAllUsers();
-	}
+    @CrossOrigin
+    @RequestMapping(value = "/users", method = GET)
+    public List<UserInfo> users() {
+        return userService.getAllUsers();
+    }
 
-	@CrossOrigin
-	@RequestMapping(value = "/userTokens/{userId}", method = GET)
-	public Set<String> userTokens(@PathVariable("userId") final Long userId) {
-		return userService.getUserTokens(userId);
-	}
+    @CrossOrigin
+    @RequestMapping(value = "/userTokens/{userId}", method = GET)
+    public Set<String> userTokens(@PathVariable("userId") final Long userId) {
+        return userService.getUserTokens(userId);
+    }
 }
--- src/main/java/org/baat/user/service/UserService.java
+++ src/main/java/org/baat/user/service/UserService.java
@@ -14,132 +14,125 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
 
-import static org.baat.user.util.Passwords.getNextSalt;
-import static org.baat.user.util.Passwords.hash;
-import static org.baat.user.util.Passwords.isExpectedPassword;
+import static org.baat.user.util.Passwords.*;
 
 @Service
 public class UserService {
 
-	@Autowired
-	UserInfoRepository userInfoRepository;
+    @Autowired
+    UserInfoRepository userInfoRepository;
 
-	@Autowired
-	UserCredentialsRepository userCredentialsRepository;
+    @Autowired
+    UserCredentialsRepository userCredentialsRepository;
 
-	@Autowired
-	UserTokenRepository userTokenRepository;
+    @Autowired
+    UserTokenRepository userTokenRepository;
 
-	public String authenticate(final UserCredentials passedCredentials) {
-		if (passedCredentials == null)
-			throw new IllegalArgumentException("User Credentials must be provided");
+    public String authenticate(final UserCredentials passedCredentials) {
+        if (passedCredentials == null)
+            throw new IllegalArgumentException("User Credentials must be provided");
 
-		if (StringUtils.isEmpty(passedCredentials.getUserName()))
-			throw new IllegalArgumentException("user name must be provided");
+        if (StringUtils.isEmpty(passedCredentials.getUserName()))
+            throw new IllegalArgumentException("user name must be provided");
 
-		if (StringUtils.isEmpty(passedCredentials.getPassword()))
-			throw new IllegalArgumentException("password must be provided");
+        if (StringUtils.isEmpty(passedCredentials.getPassword()))
+            throw new IllegalArgumentException("password must be provided");
 
-		final UserInfoEntity userInfoEntity = userInfoRepository.findByEmail(passedCredentials.getUserName());
-		if (userInfoEntity == null)
-			throw new IllegalArgumentException("Invalid user name, could not find user with such user name");
+        final UserInfoEntity userInfoEntity = userInfoRepository.findByEmail(passedCredentials.getUserName());
+        if (userInfoEntity == null)
+            throw new IllegalArgumentException("Invalid user name, could not find user with such user name");
 
-		final UserCredentialsEntity existingCredentials = userCredentialsRepository.findByUserId(userInfoEntity.getId());
-		if (existingCredentials == null)
-			throw new IllegalStateException("No credentials are stored for the user, please signup again");
+        final UserCredentialsEntity existingCredentials = userCredentialsRepository.findByUserId(userInfoEntity.getId());
+        if (existingCredentials == null)
+            throw new IllegalStateException("No credentials are stored for the user, please signup again");
 
-		if (!isExpectedPassword(passedCredentials.getPassword(),
-				existingCredentials.getSalt(),
-				existingCredentials.getPasswordHash())) {
-			throw new IllegalArgumentException("Invalid username/password");
-		}
+        if (!isExpectedPassword(passedCredentials.getPassword(),
+                existingCredentials.getSalt(),
+                existingCredentials.getPasswordHash())) {
+            throw new IllegalArgumentException("Invalid username/password");
+        }
 
-		final UserTokenEntity userToken = userTokenRepository.save(
-				new UserTokenEntity(userInfoEntity.getId(), UUID.randomUUID().toString()));
-		return userToken.getUserToken();
-	}
+        final UserTokenEntity userToken = userTokenRepository.save(
+                new UserTokenEntity(userInfoEntity.getId(), UUID.randomUUID().toString()));
+        return userToken.getUserToken();
+    }
 
-	public String signup(final SignupRequest signupRequest) {
-		if (signupRequest == null)
-			throw new IllegalArgumentException("signupRequest must be provided");
+    public String signup(final SignupRequest signupRequest) {
+        if (signupRequest == null)
+            throw new IllegalArgumentException("signupRequest must be provided");
 
-		if (StringUtils.isEmpty(signupRequest.getEmail()))
-			throw new IllegalArgumentException("email must be provided");
+        if (StringUtils.isEmpty(signupRequest.getEmail()))
+            throw new IllegalArgumentException("email must be provided");
 
-		if (StringUtils.isEmpty(signupRequest.getName()))
-			throw new IllegalArgumentException("name must be provided");
+        if (StringUtils.isEmpty(signupRequest.getName()))
+            throw new IllegalArgumentException("name must be provided");
 
-		if (StringUtils.isEmpty(signupRequest.getPassword()))
-			throw new IllegalArgumentException("password must be provided");
+        if (StringUtils.isEmpty(signupRequest.getPassword()))
+            throw new IllegalArgumentException("password must be provided");
 
-		if (userInfoRepository.findByEmail(signupRequest.getEmail()) != null)
-			throw new IllegalArgumentException("User with same email already exists");
+        if (userInfoRepository.findByEmail(signupRequest.getEmail()) != null)
+            throw new IllegalArgumentException("User with same email already exists");
 
-		final UserInfoEntity user = userInfoRepository.save(new UserInfoEntity(signupRequest.getName(),
-				signupRequest.getEmail(), signupRequest.getAvatarUrl()));
+        final UserInfoEntity user = userInfoRepository.save(new UserInfoEntity(signupRequest.getName(),
+                signupRequest.getEmail(), signupRequest.getAvatarUrl()));
 
-		final byte[] salt = getNextSalt();
-		final byte[] hash = hash(signupRequest.getPassword(), salt);
+        final byte[] salt = getNextSalt();
+        final byte[] hash = hash(signupRequest.getPassword(), salt);
 
-		userCredentialsRepository.save(
-				new UserCredentialsEntity(user.getId(),
-						user.getEmail(),
-						salt,
-						hash));
+        userCredentialsRepository.save(
+                new UserCredentialsEntity(user.getId(),
+                        user.getEmail(),
+                        salt,
+                        hash));
 
-		final UserTokenEntity userToken = userTokenRepository.save(new UserTokenEntity(user.getId(),
-				UUID.randomUUID().toString()));
-		return userToken.getUserToken();
-	}
+        final UserTokenEntity userToken = userTokenRepository.save(new UserTokenEntity(user.getId(),
+                UUID.randomUUID().toString()));
+        return userToken.getUserToken();
+    }
 
-	public boolean validateUserToken(final String userToken) {
-		if (StringUtils.isEmpty(userToken))
-			return false;
+    public boolean validateUserToken(final String userToken) {
+        if (StringUtils.isEmpty(userToken))
+            return false;
 
-		return (userTokenRepository.findByUserToken(userToken) != null);
-	}
+        return (userTokenRepository.findByUserToken(userToken) != null);
+    }
 
-	public UserInfo getUserForToken(final String userToken) {
-		UserTokenEntity userTokenEntity = userTokenRepository.findByUserToken(userToken);
-		if (userTokenEntity != null) {
-			Optional<UserInfoEntity> userInfoEntityOptional = userInfoRepository.findById( userTokenEntity.getUserId());
+    public UserInfo getUserForToken(final String userToken) {
+        UserTokenEntity userTokenEntity = userTokenRepository.findByUserToken(userToken);
+        if (userTokenEntity != null) {
+            Optional<UserInfoEntity> userInfoEntityOptional = userInfoRepository.findById(userTokenEntity.getUserId());
 
-			if (userInfoEntityOptional.isPresent()) {
-				final UserInfoEntity userInfoEntity = userInfoEntityOptional.get();
-				return new UserInfo(userInfoEntity.getId(), userInfoEntity.getEmail(),
-						userInfoEntity.getFullName(), userInfoEntity.getAvatarUrl());
-			}
-		}
-		return null;
-	}
+            if (userInfoEntityOptional.isPresent()) {
+                final UserInfoEntity userInfoEntity = userInfoEntityOptional.get();
+                return new UserInfo(userInfoEntity.getId(), userInfoEntity.getEmail(),
+                        userInfoEntity.getFullName(), userInfoEntity.getAvatarUrl());
+            }
+        }
+        return null;
+    }
 
-	public List<UserInfo> getAllUsers() {
-		List<UserInfoEntity> userInfoEntities = userInfoRepository.findAll();
-		List<UserInfo> userInfos = new ArrayList<>();
-		if (userInfoEntities != null) {
-			for (final UserInfoEntity userInfoEntity : userInfoEntities) {
-				userInfos.add(new UserInfo(userInfoEntity.getId(), userInfoEntity.getEmail(),
-						userInfoEntity.getFullName(), userInfoEntity.getAvatarUrl()));
-			}
-		}
-		return userInfos;
-	}
+    public List<UserInfo> getAllUsers() {
+        List<UserInfoEntity> userInfoEntities = userInfoRepository.findAll();
+        List<UserInfo> userInfos = new ArrayList<>();
+        if (userInfoEntities != null) {
+            for (final UserInfoEntity userInfoEntity : userInfoEntities) {
+                userInfos.add(new UserInfo(userInfoEntity.getId(), userInfoEntity.getEmail(),
+                        userInfoEntity.getFullName(), userInfoEntity.getAvatarUrl()));
+            }
+        }
+        return userInfos;
+    }
 
-	public Set<String> getUserTokens(final Long userId) {
-		final Set<UserTokenEntity> userTokens = userTokenRepository.findByUserId(userId);
-		if (userTokens == null) {
-			return Collections.emptySet();
-		}
-		return userTokens.stream()
-				.map(UserTokenEntity::getUserToken)
-				.collect(Collectors.toSet());
-	}
+    public Set<String> getUserTokens(final Long userId) {
+        final Set<UserTokenEntity> userTokens = userTokenRepository.findByUserId(userId);
+        if (userTokens == null) {
+            return Collections.emptySet();
+        }
+        return userTokens.stream()
+                .map(UserTokenEntity::getUserToken)
+                .collect(Collectors.toSet());
+    }
 }
