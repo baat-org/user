907d4f9d9dcccc17d6d5e1ab8fc8587b202c83a2
Sachin Goyal <sachin@fanduel.com>
2021-07-01 00:39:46 +0100

    Success!

--- rebuildAndPush.sh
+++ rebuildAndPush.sh
@@ -1,5 +1,15 @@
-docker rmi $(docker images -qa 'sachingoyaldocker/baat-org-user')
+SERVICE=${PWD##*/}
 
-./gradlew clean build bootJar
-docker build --no-cache -t sachingoyaldocker/baat-org-user:1.0 . 
-docker push sachingoyaldocker/baat-org-user:1.0
+# delete existing containers/images.
+docker rmi $(docker images -qa 'sachingoyaldocker/baat-org-'$SERVICE)
+echo ">>> Existing Images/Containers Deleted"
+
+# build service
+./gradlew clean build
+echo ">>> Service Jar Built"
+
+docker build --no-cache -t sachingoyaldocker/baat-org-$SERVICE:1.0 --build-arg SERVICE=$SERVICE .
+echo ">>> New Docker Image Built"
+
+docker push sachingoyaldocker/baat-org-$SERVICE:1.0
+echo ">>> New Docker Image Pushed to Docker Hub"
