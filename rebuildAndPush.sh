0607e4e3ec63b9c938fa47f54618043415611360
Sachin Goyal <sachin@fanduel.com>
2021-07-02 00:41:55 +0100

    final changes

--- rebuildAndPush.sh
+++ rebuildAndPush.sh
@@ -1,15 +1,7 @@
 SERVICE=${PWD##*/}
+VERSION=latest
 
-# delete existing containers/images.
-docker rmi $(docker images -qa 'sachingoyaldocker/baat-org-'$SERVICE)
-echo ">>> Existing Images/Containers Deleted"
-
-# build service
 ./gradlew clean build
-echo ">>> Service Jar Built"
-
-docker build --no-cache -t sachingoyaldocker/baat-org-$SERVICE:1.0 --build-arg SERVICE=$SERVICE .
-echo ">>> New Docker Image Built"
-
-docker push sachingoyaldocker/baat-org-$SERVICE:1.0
-echo ">>> New Docker Image Pushed to Docker Hub"
+docker rmi $(docker images -qa 'sachingoyaldocker/baat-org-'$SERVICE)
+docker build --no-cache -t sachingoyaldocker/baat-org-$SERVICE:$VERSION --build-arg SERVICE=$SERVICE .
+docker push sachingoyaldocker/baat-org-$SERVICE:$VERSION
---
+++ redeployLocal.sh
@@ -0,0 +1,4 @@
+kubectl delete -f infrastructure/k8s/database-deployment.yml --namespace=baat
+kubectl delete -f infrastructure/k8s/service-deployment.yml --namespace=baat
+kubectl create -f infrastructure/k8s/database-deployment.yml --namespace=baat
+kubectl create -f infrastructure/k8s/service-deployment.yml --namespace=baat
